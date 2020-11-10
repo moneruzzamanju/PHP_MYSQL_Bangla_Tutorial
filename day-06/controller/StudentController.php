acd778476f2db1753e4a12fff7b54ae51714a774
moneruzzamanju <moneruzzamanju@gmail.com>
2020-11-11 00:10:10 +0600

    PHP MySQ Bangla Tutorial 06 : Delete Student From Database

---
+++ day-06/controller/StudentController.php
@@ -0,0 +1,37 @@
+<?php
+include("../Dbconnection/dbconnection.php");
+include("../model/students.php");
+$student = new Students();
+switch ($_REQUEST['action']) {
+    case 'add':
+
+        $student->student_id = $_POST['student_id'];
+        $student->name = $_POST['name'];
+        $student->father_name = $_POST['father_name'];
+        $student->mother_name = $_POST['mother_name'];
+        $student->email = $_POST['email'];
+        $student->phone = $_POST['phone'];
+        $student->address = $_POST['address'];
+        $save = $student->save();
+        if($save){
+            $_SESSION['message'] = "<div class='alert alert-info'>Data save successfully.</div>";
+        }else{
+            $_SESSION['message'] = "<div class='alert alert-info'>Unable to save.</div>";
+
+        }
+        header('Location:../add_student.php');
+    break;
+    case 'update':
+        # code...
+        break;
+    case 'delete':
+        $id = $_REQUEST['id'];
+        $status = $student->delete($id);
+        header('Location:../index.php');
+        break;
+    default:
+        # code...
+        break;
+}
+
+?>
